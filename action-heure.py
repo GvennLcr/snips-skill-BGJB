3244807c27cabe92a500780245fe9a2fd48c6ddf
Jean-Baptiste Mériaux <jb.meriaux@live.fr>
2019-05-23 14:19:31 +0200

    modifs

--- action-heure.py
+++ action-heure.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
-from datetime import datetime
-from pytz import timezone
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
-
-		hermes.publish_end_session(intent_message.session_id, "Ping !")
+	hermes.publish_end_session(intent_message.session_id, "Pong !")
 
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
