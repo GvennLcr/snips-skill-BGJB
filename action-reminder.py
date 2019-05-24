037f42e8f4dda15f40628732c5d6604a64d5bbb0
Jean-Baptiste Mériaux <jb.meriaux@live.fr>
2019-05-24 16:25:22 +0200

    test API

--- action-reminder.py
+++ action-reminder.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 from hermes_python.hermes import Hermes
-#from datetime import datetime
-#from pytz import timezone
+from datetime import datetime
+from pytz import timezone
+from collections import namedtuple
+import http.client
+import json
+import Patient
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
@@ -9,12 +13,17 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
 	reminder_msg = ""
-	#now = datetime.now(timezone('Europe/Paris'))
+	now = datetime.now(timezone('Europe/Paris'))
 
 	try:
-		raise ValueError('invalid user response')
+		apiConnection = http.client.HTTPConnection('http://vouvouf.eu:8080/api/patients/1')
+		apiResponse = apiConnection.getresponse()
+
+		patient = json.loads(apiResponse.read(), object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+		reminder_msg = "Le patient s'appelle {0} {1}".format(patient.FirstName, patient.LastName)
 	except:
-		reminder_msg = "Bouffe moi le cul."
+		reminder_msg = "ça ne marche pas"
 
 	hermes.publish_end_session(intent_message.session_id, reminder_msg)
 
---
+++ patient.py
@@ -0,0 +1,9 @@
+class Patient:
+    def __init__(self, id, firstName, lastName, birthdate, address, voucher, phone):
+        self.Id = id
+        self.FirstName = firstName
+        self.LastName = lastName
+        self.Birthdate = birthdate
+        self.Address = address
+        self.Voucher = voucher
+        self.Phone = phone
