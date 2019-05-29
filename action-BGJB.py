e143c02a12e46559ff7e0e14d3eaad2db1d6010f
Jean-Baptiste Mériaux <jb.meriaux@live.fr>
2019-05-29 14:31:35 +0200

    ok ?

--- action-BGJB.py
+++ action-BGJB.py
@@ -12,26 +12,17 @@ MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 now = datetime.now(timezone('Europe/Paris'))
 headers = {'Content-Type': 'application/json'}
-reminder_msg = ""
 is_exception_raised = False
 #pdb.set_trace()
-
-"""def execute_action_from_intentName(intentName):
-    switcher = {
-        "DiiagePFC:AskPatientInfos": patient_info,
-        "DiiagePFC:PillsReminder": patient_info,
-    }
-    # Get the function from switcher dictionary
-    func = switcher.get(intentName, lambda: "Invalid intentName")
-    # Execute the function
-    func()"""
 
 
 def patient_info_handler(hermes, intent_message):
 	try:
+		patient_info = ""
+		print(intent_message)
 		if intent_message.slots.count() == 0:
 			raise ValueError('No slot value found')
-		
+
 		patientId = int(intent_message.slots.PatientId.first().value)
 		print(patientId)
 
@@ -41,32 +32,23 @@ def patient_info_handler(hermes, intent_message):
 		if apiResponse.status_code == 200:
 			patient = json.loads(apiResponse.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
 			print(patient)
-			reminder_msg = "Le patient numéro {} s appelle {} {}".format(patientId, patient.firstName, patient.lastName)
+			patient_info = "Le patient numéro {} s appelle {} {}".format(patientId, patient.firstName, patient.lastName)
 		else:
 			raise ConnectionError #Mettre à jour l'API après une modif dans la BDD ?
 
 	except ConnectionError:
-		reminder_msg = "Désolé, je ne parviens pas à récupérer les informations demandées."
+		patient_info = "Désolé, je ne parviens pas à récupérer les informations demandées."
 	except json.JSONDecodeError:
-		reminder_msg = "Désolé, il y a un soucis, je ne parviens à interpréter les informations demandées."
+		patient_info = "Désolé, il y a un soucis, je ne parviens à interpréter les informations demandées."
 	except ValueError:
-		reminder_msg = "Excusez-moi, je n'ai pas compris de quel patient vous parlez, pouvez-vous répéter son numéro ?"
-		hermes.publish_continue_session(intent_message.session_id, reminder_msg, ["PillsReminder"], slot_to_fill=json.dumps("PatientId"))
+		patient_info = "Excusez-moi, je n'ai pas compris de quel patient vous parlez, pouvez-vous répéter son numéro ?"
+		hermes.publish_continue_session(intent_message.session_id, patient_info, ["PillsReminder"], slot_to_fill=json.dumps("PatientId"))
 	else:
-		hermes.publish_end_session(intent_message.session_id, reminder_msg)
+		hermes.publish_end_session(intent_message.session_id, patient_info)
 	finally:
-		print("DEBUG : reminder_msg = " + reminder_msg)
-
-
-"""def pills_reminder_handler(hermes, intent_message):
-	if reminder_msg == "":
-		reminder_msg = "Désolé, un erreur est survenue, pouvez-vous répéter ou reformuler votre phrase s'il vous plaît ?" """
+		print("DEBUG : patient_info = " + patient_info)
 
 
 with Hermes(MQTT_ADDR) as h:
 	print("test")
-	#intent_received(None, '{"input": "comment s\' appelle le patient numéro un","intent": {"intentName": "DiiagePFC:AskPatientInfos","confidenceScore": 1},"slots": [{"rawValue": "un","value": {"kind": "Number","value": 1},"range": {"start": 37,"end": 39},"entity": "snips/number","slotName": "PatientId"}]}')
-	h\
-		.subscribe_intents("AskPatientInfos", patient_info_handler)\
-		.start()
-		#.subscribe_intents("PillsReminder", pills_reminder_handler)\
+	h.subscribe_intents('DiiagePFC:AskPatientInfos', patient_info_handler).start()
